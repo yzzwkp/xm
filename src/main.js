fc7353e7a98ed85a5f87ba6b432484981d19c2b5
yzzwkp <50953189+yzzwkp@users.noreply.github.com>
2019-07-08 22:08:36 +0800

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+
+import'mint-ui/lib/style.css'
+
+Vue.config.productionTip = false
+//5: 设置请求的根路径 
+//Vue.http.options.root = "http://127.0.0.1/vue_ser/";
+//6:全局设置post 时候表音的数据组织格式为 application/x-www-form-urlencoded
+//Vue.http.options.emulateJSON = true;
+// 导入 MUI 的样式表， 和 Bootstrap 用法没有差别
+import './lib/mui/css/mui.css'
+// 导入 MUI 的样式表，扩展图标样式，购物车图标
+// 还需要加载图标字体文件
+import './lib/mui/css/icons-extra.css'
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
