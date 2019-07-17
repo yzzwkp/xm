6d4e5cafbd4fee414a64ad8bc77a665d8e748f4c
yzzwkp <50953189+yzzwkp@users.noreply.github.com>
2019-07-17 22:55:46 +0800

    Add files via upload

--- shop/vue_app_00/src/components/words/yzzhome.vue
+++ shop/vue_app_00/src/components/words/yzzhome.vue
@@ -1,23 +1,18 @@
 <template>
     <div>
       <div class="Recommend">
-          每日好句推荐
+          每日推荐
       </div>
       <!-- 轮播图 -->
       <div class="Sowing">
           <div id="left1">
-          <ul id="play-lun">
-            <li><img src="../../assets/01.png" alt=""></li>
-            <li><img src="../../assets/02.png" alt=""></li>
-            <li><img src="../../assets/03.png" alt=""></li>
-            <li><img src="../../assets/04.png" alt=""></li>
-          </ul>
+      <homepage></homepage>
         </div>
       </div>
        <!-- 搜索框 -->
         <div class="search1">
       <input type="text" placeholder="请输入单词搜索" class="search">
-      <button class="btn"><img src="" alt=""></button>
+      <button class="btn"></button>
       </div>
       <!-- 搜索内容 -->
       <div class="search_d">
@@ -36,13 +31,17 @@
 </template>
 
 <script>
+import HomePage from './HomePage';
     export default {
         data(){
             return {}
         },
         methods:{
-     
-        }
+      
+        },
+        components:{
+           'homepage':HomePage
+       },
     }
 </script>
 <style scoped>
@@ -57,7 +56,7 @@
     border: 1px solid brown;
     width: 99%;
     height: 60px;
-    background-color: rgb(126,178,248);
+    background-color: #f9940c;
     border-radius: 11px;
     margin: 10px 0;
 }
@@ -66,33 +65,39 @@
         height: 37px;
         outline: none;
         margin-top: 10px;
-        border-radius: 10px;
+        border:0;
     }
     /* 搜索按钮 */
     .btn{
       width: 60px;
-      height: 41px;
+      height: 37px;
       position: absolute;
       margin-top: 10px;
       border-radius: 5px;
       outline: none;
+      background-image:url("../../assets/soushuo1.png");
+      background-size:46px 31px;
+      background-repeat:no-repeat;
     }
     .search_d{
-        border: 1px solid red;
+        border: 1px solid #f9940c;
         width: 99%;
-        height: 170px;
+        height: 230px;
+        top:300px;
+        position:absolute;
+        top:375px;
     }
     .search_d>div:nth-child(1){
-        border: 1px solid red;
+        border: 1px solid #f9940c;
         width: 49%;
-        height:170px;
+        height:230px;
         float: left;
     }
     .search_d>div:nth-child(1)>div>p{
         width: 100%;
         height: 100%;
        text-align: center;
-       line-height: 60px;
+       line-height: 85px;
        color: blue;
     }
      .search_d>div:nth-child(1)>p:nth-child(1){
@@ -101,26 +106,26 @@
          margin: 0 auto;
      }
     .search_d>div:nth-child(1)>div:nth-child(2){
-        border: 1px solid blue;
+        border: 1px solid #f9940c;
         width: 90%;
-        height: 60px;
+        height: 85px;
         margin: 0 auto;
         margin-top: 10px;
         border-radius: 11px;
     }
     .search_d>div:nth-child(1)>div:nth-child(3){
-        border: 1px solid blue;
+        border: 1px solid #f9940c;
         width: 90%;
-        height: 60px;
+        height: 85px;
         margin: 0 auto;
         margin-top: 10px;
         border-radius: 11px;
     }
     /* 搜索内容右侧div */
     .search_d>div:nth-child(2){
-        border: 1px solid red;
+        border: 1px solid #f9940c;
         width: 49%;
-        height:170px;
+        height:230px;
         float: right;
     }
     .search_d>div:nth-child(2)>img{
@@ -131,25 +136,26 @@ height: 100;
     .Recommend{
        width: 99%;
         height: 40px;
-        border: 1px solid red;
+        border: 3px solid #f9940c;
+        border-radius:11px;
         margin: 0 auto;
         color:blue;
         text-align: center;
-        line-height: 40px;
+        line-height: 36px;
         
     }
     /* 轮播图 */
     .Sowing{
          width: 99%;
-        height: 157px;
-        border: 1px solid red;
+        height: 200px;
+        border: 1px solid #f9940c;
         margin: 0 auto;
         margin-top: 10px;
-         overflow: hidden;
+        overflow: hidden;
     }
    #left1{
     width: 99%;
-    height: 100%;
+    height: 30%;
     overflow: hidden;
     position: fixed;
     float: left;
@@ -165,6 +171,6 @@ height: 100;
 #play-lun>li>img{
 width: 356px;
 height: 158px;
-border: 1px solid hotpink;
+border: 1px solid #f9940c;
 }
 </style>
